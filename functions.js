ec68668f085fe758847f8c8e68adbdcc23bb7499
shokone <imartineztutor@gmail.com>
2016-02-15 13:45:49 +0100

    actualizamos ficheros

--- functions.js
+++ functions.js
@@ -1,28 +1,36 @@
 function compAcceso(){
-	var user = document.getElementById("nombre").value;
-	var pass = document.getElementById("password").value;
-
+	var user = document.getElementById("nombre");
+	var pass = document.getElementById("password");
+	if(user.value == "ivan" && pass.value == "ivan"){
+		window.location.href="#menuProfesor";
+	} 
 }
 
 function camposCursoAlumno(){
 	var curso = document.getElementById("selCurso");
 	var alumno = document.getElementById("selAlumno");
-	var cursos = ['DAW', 'DAM'];
-	var alumnos = [
-		'Marcos Lopez', 'Arkaitz Huerta', 
-		'Eneko irarragorri', 'Iván Martínez', 
-		'Sergio Blasco'
-	];
 	//añadimos los diferentes cursos
+	var cursos = ['DAW', 'DAM'];
 	for(var a = 0;a<cursos.length;a++){
 		var option = document.createElement("option");
-		option.value=cursos[a];
+		option.innerHTML=cursos[a];
+		option.value = cursos[a];
 		curso.appendChild(option);
 	}
 	//añadimos los diferentes alumnos
+	var alumnos = [
+		'Iván Martínez', 'Marcos Lopez', 'Arkaitz Huerta', 
+		'Eneko irarragorri', 'Sergio Blasco'
+	];
 	for(var a = 0;a<alumnos.length;a++){
 		var option = document.createElement("option");
+		option.innerHTML=alumnos[a];
 		option.value=alumnos[a];
-		curso.appendChild(option);
+		alumno.appendChild(option);
 	}
+}
+
+function evaluacion(){
+	var curso = document.getElementById("selCurso");
+	var alumno = document.getElementById("selAlumno");
 }
